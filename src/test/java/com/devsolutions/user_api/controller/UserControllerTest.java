080811c1a642a483f0798dba898a4b1052f24e7b
Brandon Arroyo <brandon.arroyo.2000@gmail.com>
2026-06-16 00:32:11 -0600

    [T-005] Finalize test configuration and restore UserServiceTest

--- src/test/java/com/devsolutions/user_api/controller/UserControllerTest.java
+++ src/test/java/com/devsolutions/user_api/controller/UserControllerTest.java
@@ -1,23 +1,26 @@
 package com.devsolutions.user_api.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.devsolutions.user_api.dto.UserRequestDTO;
-import com.devsolutions.user_api.repository.UserRepository;
-
+import static org.hamcrest.Matchers.containsString;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.devsolutions.user_api.dto.UserRequestDTO;
+import com.devsolutions.user_api.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class UserControllerTest {
     
     @Autowired
