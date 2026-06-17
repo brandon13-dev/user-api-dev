8381dc63f8271c72768a7c06a502137726ade11b
Brandon Arroyo <brandon.arroyo.2000@gmail.com>
2026-06-17 00:28:19 -0600

    [T-006] Fix test configuration: ensure test profile is used in CI

--- src/test/java/com/devsolutions/user_api/service/UserServiceTest.java
+++ src/test/java/com/devsolutions/user_api/service/UserServiceTest.java
@@ -2,7 +2,9 @@ package com.devsolutions.user_api.service;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 public class UserServiceTest {
 }
